110763a3b89c502142870a5e4deef23665080705
Amol M Deshpande <amol.deshpande@in.ibm.com>
2021-07-09 15:25:40 +0530

    deployment changes

--- server.js
+++ server.js
@@ -20,8 +20,6 @@ app.get('/add', (req, res) => {
 app.get('/sub', (req, res) => {
     let param1 = req.query.param1;
     let param2 = req.query.param2;
-      console.log("Request add : param1  : ",param1," param2  : ",param2)
-
     let result = parseInt(param1) - parseInt(param2)
     res.json({result: result+''})
 })
@@ -29,8 +27,6 @@ app.get('/sub', (req, res) => {
 app.get('/mul', (req, res) => {
     let param1 = req.query.param1;
     let param2 = req.query.param2;
-      console.log("Request add : param1  : ",param1," param2  : ",param2)
-
     let result = parseInt(param1) * parseInt(param2)
     res.json({result: result+''})
   })
@@ -40,3 +36,16 @@ app.get('/mul', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+//oc deployment backup
+// oc create deployment sample-node-app --image=amoldeshpande/sample-node-app:latest
+// oc expose deployment sample-node-app --type="NodePort" --port=8080
+// oc expose service sample-node-app
+// oc get routes
+
+
+
+// oc apply -f deployment.yaml
+// oc apply -f service.yaml
+// oc apply -f route.yaml
+// oc get routes
